ec32c77e41c9de1e38a3c0b384042cace37284f7
jesus Dario Marenco <jesusdario10@hotmail.com>
2017-10-12 14:52:19 -0500

    landing del signup listo y funcionando

--- public/js/app.js
+++ public/js/app.js
@@ -2627,7 +2627,25 @@ page('/', function (ctx, next) {
 
 page();
 
-},{"./signup":18,"page":12}],18:[function(require,module,exports){
+},{"./signup":19,"page":12}],18:[function(require,module,exports){
+var yo = require('yo-yo');
+
+module.exports = function landing(box) {
+  return yo`<div class="container" >
+    <div class="row">
+      <div class="col s10 push-s1">
+        <div class="row">
+          <div class="col m5 hide-on-small-only">
+              <img src="iphone.png" class="iphone" alt="">
+          </div>
+          ${box}
+        </div>
+      </div>
+    </div>
+  </div>`;
+};
+
+},{"yo-yo":15}],19:[function(require,module,exports){
 var page = require('page');
 var template = require('./template.js');
 var empty = require('empty-element');
@@ -2637,46 +2655,38 @@ page('/signup', function (req, res) {
 	empty(main).appendChild(template);
 });
 
-},{"./template.js":19,"empty-element":3,"page":12}],19:[function(require,module,exports){
+},{"./template.js":20,"empty-element":3,"page":12}],20:[function(require,module,exports){
 var yo = require('yo-yo');
+var landing = require('../landing');
 
-module.exports = yo`<div class="container" >
+var signupForm = yo`<div class="col s12 m7">
   <div class="row">
-    <div class="col s10 push-s1">
-      <div class="row">
-        <div class="col m5 hide-on-small-only">
-            <img src="iphone.png" class="iphone" alt="">
+    <div class="signup-box">
+      <h1 class="platzigram">Platzigramupoptu</h1>
+      <form action="" class="signup-form">
+        <h2>Registrate</h2>
+        <div class="section">
+          <a href="" class="btn btn-fb waves-effect waves-light hide-on-small-only">Iniciar Sesion con facebook</a><!--la clase hide-on-small-only hace que no se muestre el a en dispositivos moviles-->
+          <a href="" class="btn btn-signup hide-on-med-and-up">Iniciar Sesion</a><!-- la clase hide-on-med-and-up hace que se muestre en dispositivos moviles-->
         </div>
-        <div class="col s12 m7">
-          <div class="row">
-            <div class="signup-box">
-              <h1 class="platzigram">Platzigramupop</h1>
-              <form action="" class="signup-form">
-                <h2>Registrate</h2>
-                <div class="section">
-                  <a href="" class="btn btn-fb waves-effect waves-light hide-on-small-only">Iniciar Sesion con facebook</a><!--la clase hide-on-small-only hace que no se muestre el a en dispositivos moviles-->
-                  <a href="" class="btn btn-signup hide-on-med-and-up">Iniciar Sesion</a><!-- la clase hide-on-med-and-up hace que se muestre en dispositivos moviles-->
-                </div>
-                <div class="divider"></div>
-                <div class="section">
-                  <input type="email" name="email" placeholder="Correo Electronico" value="">
-                  <input type="text" name="name" placeholder="Nombre completo" value="">
-                  <input type="text" name="username" placeholder="Nombre de usuario" value="">
-                  <input type="password" name="password" placeholder="Password" value="">
-                  <button type="submit" class="btn btn-signup waves-effect waves-light">Iniciar Sesion</button>
-                </div>
-              </form>
-            </div>
-          </div>
-          <div class="row">
-            <div class="login-box">
-              ¿Tienes una cuenta? <a href="/signin">Entrar</a>
-            </div>
-          </div>
+        <div class="divider"></div>
+        <div class="section">
+          <input type="email" name="email" placeholder="Correo Electronico" value="">
+          <input type="text" name="name" placeholder="Nombre completo" value="">
+          <input type="text" name="username" placeholder="Nombre de usuario" value="">
+          <input type="password" name="password" placeholder="Password" value="">
+          <button type="submit" class="btn btn-signup waves-effect waves-light">Iniciar Sesion</button>
         </div>
-      </div>
+      </form>
+    </div>
+  </div>
+  <div class="row">
+    <div class="login-box">
+      ¿Tienes una cuenta? <a href="/signin">Entrar</a>
     </div>
   </div>
 </div>`;
 
-},{"yo-yo":15}]},{},[17]);
+module.exports = landing(signupForm);
+
+},{"../landing":18,"yo-yo":15}]},{},[17]);
--- src/signup/template.js
+++ src/signup/template.js
@@ -1,40 +1,32 @@
 var yo = require('yo-yo');
+var landing = require('../landing');
 
-module.exports=yo`<div class="container" >
+var signupForm = yo`<div class="col s12 m7">
   <div class="row">
-    <div class="col s10 push-s1">
-      <div class="row">
-        <div class="col m5 hide-on-small-only">
-            <img src="iphone.png" class="iphone" alt="">
+    <div class="signup-box">
+      <h1 class="platzigram">Platzigramupoptu</h1>
+      <form action="" class="signup-form">
+        <h2>Registrate</h2>
+        <div class="section">
+          <a href="" class="btn btn-fb waves-effect waves-light hide-on-small-only">Iniciar Sesion con facebook</a><!--la clase hide-on-small-only hace que no se muestre el a en dispositivos moviles-->
+          <a href="" class="btn btn-signup hide-on-med-and-up">Iniciar Sesion</a><!-- la clase hide-on-med-and-up hace que se muestre en dispositivos moviles-->
         </div>
-        <div class="col s12 m7">
-          <div class="row">
-            <div class="signup-box">
-              <h1 class="platzigram">Platzigramupop</h1>
-              <form action="" class="signup-form">
-                <h2>Registrate</h2>
-                <div class="section">
-                  <a href="" class="btn btn-fb waves-effect waves-light hide-on-small-only">Iniciar Sesion con facebook</a><!--la clase hide-on-small-only hace que no se muestre el a en dispositivos moviles-->
-                  <a href="" class="btn btn-signup hide-on-med-and-up">Iniciar Sesion</a><!-- la clase hide-on-med-and-up hace que se muestre en dispositivos moviles-->
-                </div>
-                <div class="divider"></div>
-                <div class="section">
-                  <input type="email" name="email" placeholder="Correo Electronico" value="">
-                  <input type="text" name="name" placeholder="Nombre completo" value="">
-                  <input type="text" name="username" placeholder="Nombre de usuario" value="">
-                  <input type="password" name="password" placeholder="Password" value="">
-                  <button type="submit" class="btn btn-signup waves-effect waves-light">Iniciar Sesion</button>
-                </div>
-              </form>
-            </div>
-          </div>
-          <div class="row">
-            <div class="login-box">
-              ¿Tienes una cuenta? <a href="/signin">Entrar</a>
-            </div>
-          </div>
+        <div class="divider"></div>
+        <div class="section">
+          <input type="email" name="email" placeholder="Correo Electronico" value="">
+          <input type="text" name="name" placeholder="Nombre completo" value="">
+          <input type="text" name="username" placeholder="Nombre de usuario" value="">
+          <input type="password" name="password" placeholder="Password" value="">
+          <button type="submit" class="btn btn-signup waves-effect waves-light">Iniciar Sesion</button>
         </div>
-      </div>
+      </form>
     </div>
   </div>
-</div>`
+  <div class="row">
+    <div class="login-box">
+      ¿Tienes una cuenta? <a href="/signin">Entrar</a>
+    </div>
+  </div>
+</div>`;
+
+module.exports=landing(signupForm)

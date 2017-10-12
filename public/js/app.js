4068afe06351cf729a97eea40a71d48230d81deb
jesus Dario Marenco <jesusdario10@hotmail.com>
2017-10-12 15:31:45 -0500

    arreglando las vistas signin y signup tenian problemas de colores al cambiar tamaño

--- public/js/app.js
+++ public/js/app.js
@@ -2686,21 +2686,21 @@ var signinForm = yo`<div class="col s12 m7">
       <form action="" class="signup-form">
         <h2>Signin</h2>
         <div class="section">
-          <a href="" class="btn btn-fb waves-effect waves-light hide-on-small-only">Iniciar Sesion con facebook</a><!--la clase hide-on-small-only hace que no se muestre el a en dispositivos moviles-->
-          <a href="" class="btn btn-signup hide-on-med-and-up">Iniciar Sesion</a><!-- la clase hide-on-med-and-up hace que se muestre en dispositivos moviles-->
+          <a href="" class="btn btn-fb waves-effect waves-light hide-on-small-only"><i class="fa fa-facebook-square" aria-hidden="true"></i>  Iniciar Sesion con facebook</a><!--la clase hide-on-small-only hace que no se muestre el a en dispositivos moviles-->
+          <a href="" class="btn btn-fb btn-signup hide-on-med-and-up">Iniciar Sesion</a><!-- la clase hide-on-med-and-up hace que se muestre en dispositivos moviles-->
         </div>
         <div class="divider"></div>
         <div class="section">
           <input type="text" name="username" placeholder="Nombre de usuario" value="">
           <input type="password" name="password" placeholder="Password" value="">
-          <button type="submit" class="btn btn-signup waves-effect waves-light">Iniciar Sesion</button>
+          <button type="submit" class="btn btn-fb btn-signup waves-effect waves-light">Iniciar Sesion</button>
         </div>
       </form>
     </div>
   </div>
   <div class="row">
     <div class="login-box">
-      ¿Tienes una cuenta? <a href="/signup">Entrar</a>
+      ¿Tienes una cuenta? <a href="/signin">Entrar</a>
     </div>
   </div>
 </div>`;
@@ -2730,7 +2730,7 @@ var signupForm = yo`<div class="col s12 m7">
         <h2>Registrate</h2>
         <div class="section">
           <a href="" class="btn btn-fb waves-effect waves-light hide-on-small-only"><i class="fa fa-facebook-square" aria-hidden="true"></i>  Iniciar Sesion con facebook</a><!--la clase hide-on-small-only hace que no se muestre el a en dispositivos moviles-->
-          <a href="" class="btn btn-fb btn-signup hide-on-med-and-up"><i class="fa fa-facebook-square" aria-hidden="true"></i>  Iniciar Sesion</a><!-- la clase hide-on-med-and-up hace que se muestre en dispositivos moviles-->
+          <a href="" class="btn btn-fb btn-signup hide-on-med-and-up">Iniciar Sesion</a><!-- la clase hide-on-med-and-up hace que se muestre en dispositivos moviles-->
         </div>
         <div class="divider"></div>
         <div class="section">

b4e4687a21a68710b5a33319850d67ca49f85eaa
jesus Dario Marenco <jesusdario10@hotmail.com>
2017-10-12 10:38:46 -0500

    creando y probando el servidor express en server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app= express();
+
+app.get('/', function(req, res)
+{
+  res.send('hola')
+})
+app.listen(3000, function(err)
+{
+  if(err){console.log('errorrr')}
+  console.log('exito servidor conectado')
+})
